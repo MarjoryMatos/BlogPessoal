1d9bbd14f5bbd3d897664502983a1258cb574b7e
MarjoryMatos <marjory_matos@hotmail.com>
2022-05-09 14:54:06 -0300

    adicionando model usuário

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -50,13 +50,30 @@ public class PostagemController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem)); 
 	}
 	
-	@PutMapping
+	/*@PutMapping
 	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem)); 
+	}*/
+	
+	@PutMapping
+	public ResponseEntity <Postagem> put(@RequestBody Postagem postagem){
+		return repository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.ok().body(repository.save(postagem)))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@DeleteMapping("/{id}")
+	/*@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
+	}*/
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+		return repository.findById(id)
+				.map(resposta -> {
+					repository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
