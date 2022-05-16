4ddbb707f78a929b892c13f918a9aec0f95cd057
MarjoryMatos <marjory_matos@hotmail.com>
2022-05-16 15:14:57 -0300

    deploy no heroku

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
@@ -27,7 +29,7 @@ public class Usuario {
 	@NotNull(message = "O nome é obrigatório")
 	private String nome;
 	
-	
+	@Schema(example= "email@email.com.br")
 	@NotBlank(message = "O e-mail é obrigatório")
 	@Email(message = "O e-mail deve ser um e-mail válido!")
 	private String emailUsuario;
